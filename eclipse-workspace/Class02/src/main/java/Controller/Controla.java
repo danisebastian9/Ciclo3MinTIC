ffe87b839a677b23f1f40a7c517afa7abac73389
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-09-12 23:31:07 -0500

    Class02 added

---
+++ eclipse-workspace/Class02/src/main/java/Controller/Controla.java
@@ -0,0 +1,45 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+/**
+ * Servlet implementation class Controla
+ */
+@WebServlet("/Controla")
+public class Controla extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Controla() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int ladoA, ladoB, ladoC;
+		String tipo = "";
+		
+		if(request.getParameter("enviar")!= null) {
+			ladoA = Integer.parseInt(request.getParameter("ladoA"));
+			ladoB = Integer.parseInt(request.getParameter("ladoB"));
+			ladoC = Integer.parseInt(request.getParameter("ladoC"));
+			
+			if(ladoA == ladoB && ladoB == ladoC) {
+				tipo = "Triangulo Equilatero";
+			} else if(ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+				tipo = "Triangulo Isoceles";
+			} else {
+				tipo = "Triangulo Escaleno";
+			}
+			JOptionPane.showMessageDialog(null, tipo);
+			
+			response.sendRedirect("Index.jsp?t=" + tipo);
+		}
+
+	}
+
+}
---
+++ eclipse-workspace/Class02/src/main/webapp/Index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Test Application	</title>
+</head>
+<body>
+<form action="Controla" method="POST">
+    <div><label for="">Lado A:</label><input type="number" name="ladoA"></div>
+    <div><label for="">Lado B:</label><input type="number" name="ladoB"></div>
+    <div><label for="">Lado C:</label><input type="number" name="ladoC"></div>
+    <div><input type="submit" name="enviar"></div>
+    <hr>
+    <%
+    String res = request.getParameter("t"); 
+    %>
+    <div> Respuesta: <%=res %> </div>
+</form>	
+</body>
+</html>
